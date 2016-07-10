5d12e6876f14564471f9237ee6f5c3d92f54144d
Nicolas Chalet <nicolas.chalet@gmail.com>
2016-07-10 15:28:49 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Slides
+
+Soon ...
+
+## License
+
+MIT licensed
+
+Copyright (C) 2016 Nicolas CHALET, https://github.com/Grobim
---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+(function() {
+  'use strict';
+
+  var detailTarget = document.getElementById('detailsTarget'),
+    details = {
+      nom : 'Nicolas',
+      prenom : 'CHALET',
+      company : 'Extia',
+      currentClient : 'PMU',
+      profession : 'Full-Stack software engineer',
+      centerOfInterests : [
+        'Web developpement',
+        'Java/Javascript ♥♥♥',
+        'Music player',
+        'Video games'
+      ]
+    };
+
+  document.body.addEventListener("contextmenu", function(event) {
+    event.preventDefault();
+    Reveal.next();
+    return false;
+  });
+
+  setInterval(function() {
+    details.age = moment().diff(moment([1988, 5, 21]).hour(5).minute(20));
+
+    detailTarget.innerHTML = JSON.stringify(details, null, 2);
+    hljs.highlightBlock(detailTarget);
+  }, 500);
+
+})();
